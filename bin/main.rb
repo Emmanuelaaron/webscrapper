8963dd9f20ed1449342875fe247fb4aa6d1c8a1a
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-06-07 19:28:53 +0300

    Adds Scrapper class to the layout

---
+++ bin/main.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/scrapper'
+
+puts '-----------------------------------'
+puts 'Welcome to Jumia webScrapper'
+puts '------------------------------------'
+puts 'What would you like to buy'
+puts '---------------------------------------'
+product = gets .chomp
+my_scrapper = Scrapper.new(product)
+
+p my_scrapper.my_results
