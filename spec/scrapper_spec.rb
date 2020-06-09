6469bff3a170bea77874f1ac8cc617f06ca99671
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-06-09 19:02:05 +0300

    Adds rspec test for parser method

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -5,12 +5,11 @@ require 'HTTParty'
 describe 'Scrapper' do
   let(:my_scrapper) { Scrapper.new('phones') }
   let(:jumia_url) { 'https://www.jumia.ug//catalog/?q=phones&page=1' }
-  # let(:my_parsed_url) { Nokogiri::HTML(HTTParty.get(jumia_url)) }
-  # describe '#parser' do
-  #   it 'returns the url of jumia website' do
-  #     expect(my_scrapper.parser).to eql(my_parsed_url)
-  #   end
-  # end
+  describe '#parser' do
+    it 'return nokogiri' do
+      expect(my_scrapper.parser).to be_truthy
+    end
+  end
   describe '#my_url' do
     it 'returns the url to be scrapped' do
       expect(my_scrapper.my_url(1)).to eql(jumia_url)
