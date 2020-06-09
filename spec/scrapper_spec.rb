92b99f7b66598f559e618c6e120946c9338644e1
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-06-09 18:23:12 +0300

    Updates workflows folder and solves rubocop errors

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -4,12 +4,11 @@ require 'HTTParty'
 
 describe 'Scrapper' do
   let(:my_scrapper) { Scrapper.new('phones') }
-  let(:jumia_url) {'https://www.jumia.ug/catalog/?q=phones&page=1'}
+  let(:jumia_url) { 'https://www.jumia.ug/catalog/?q=phones&page=1' }
   let(:my_parsed_url) { Nokogiri::HTML(HTTParty.get(jumia_url)) }
   describe '#parser' do
-    it 'returns the url of jumia website'do
+    it 'returns the url of jumia website' do
       expect(my_scrapper.parser).to eql(my_parsed_url)
+    end
   end
-  end
-  
 end
